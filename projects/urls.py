24680a88156825a44998450568d068e9865f0a66
unknown <taimaraaladejo@gmail.com>
2025-01-11 11:54:06 -0400

    detail listo

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from projects.views import ListProjects
+from projects.views import ListProjects,DetailProjects
 urlpatterns=[
 
-    path("",ListProjects.as_view())
+    path("",ListProjects.as_view()),
+    path("detail/<int:pk>",DetailProjects.as_view(),name="detail")
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView 
 from  projects.models import post
+
 # Create your views here.
 
 class ListProjects(ListView):
     model=post 
     template_name="home.html"
 
+class DetailProjects(DetailView):
+    model=post
+    template_name="detail.html"
